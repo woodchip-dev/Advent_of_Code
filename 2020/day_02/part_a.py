607d21698d62a429e9682765c4abdaaf6469acf8
delmitch <amitchell@alaska.edu>
2020-12-01 21:43:08 -0900

    Feat: adds day_02 part_a from template

---
+++ 2020/day_02/part_a.py
@@ -0,0 +1,16 @@
+import utils
+
+# fetch data
+data = utils.line_to_list('input_files/aoc_02.txt')
+
+### START SOLUTION BODY ###
+
+# find how many passwords are valid according to their respective policy
+found, answer = False, None
+# add code here
+
+### END SOLUTION BODY ###
+
+# output result
+utils.one_val_out(found, answer)
+
