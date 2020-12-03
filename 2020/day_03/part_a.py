1a07ab2e3f66b836b4a757fbcca97ea6b69c6175
delmitch <amitchell@alaska.edu>
2020-12-02 21:39:49 -0900

    Feat: solves day 03 part a

---
+++ 2020/day_03/part_a.py
@@ -0,0 +1,23 @@
+import utils
+
+# fetch data
+data = utils.line_to_list('input_files/aoc_03.txt')
+
+### START SOLUTION BODY ###
+
+# find how many collisions with trees there'd be while travelling R3,D1
+found, answer = False, 0
+width, height, x = len(data[0]), len(data), 0
+for y in range(height):
+    if data[y][x] == '#':
+        answer = answer + 1
+    x = x + 3
+    if x >= width:
+        x = x - width
+found = True
+
+### END SOLUTION BODY ###
+
+# output result
+utils.one_val_out(found, answer)
+
