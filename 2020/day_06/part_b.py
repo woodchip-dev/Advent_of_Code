e61a49258b5595122e00ba097e60aa57356f940a
delmitch <amitchell@alaska.edu>
2020-12-05 20:25:48 -0900

    Feat: solves day 06 part b

--- 2020/day_06/part_b.py
+++ 2020/day_06/part_b.py
@@ -1,13 +1,20 @@
 import utils
 
 # fetch data
-data = utils.line_to_list('input_files/aoc_xx.txt')
+data = utils.separated_by_blanks('input_files/aoc_06.txt')
 
 ### START SOLUTION BODY ###
 
-# task overview comment
-found, answer = False, None
-# add code here
+# find the sum of the unanimous 'yes' answers from each group
+found, answer = False, 0
+for item in data:
+    count = len(item.split(' '))
+    item = item.replace(' ', '')
+    item = [ch for ch in item]
+    for ch in set(item):
+        if item.count(ch) == count:
+            answer += 1
+found = True
 
 ### END SOLUTION BODY ###
 
