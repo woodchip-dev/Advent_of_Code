7c7f1a76dff9240a386d0967d723ceee258c23ba
delmitch <amitchell@alaska.edu>
2020-12-08 21:45:28 -0900

    Feat: solves day 09 part a

--- 2020/day_09/part_a.py
+++ 2020/day_09/part_a.py
@@ -1,13 +1,23 @@
 import utils
 
 # fetch data
-data = utils.line_to_list('input_files/aoc_xx.txt')
+data = utils.lines_to_ints('input_files/aoc_09.txt')
 
 ### START SOLUTION BODY ###
 
-# task overview comment
-found, answer = False, None
-# add code here
+def two_sum_next(nums, target):
+    for n in nums:
+        if target - n in nums:
+            return True
+    return False
+
+# find the first number that is not the sum of 2 of the 25 numbers before it
+found, answer = False, 0
+for idx in range(len(data) - 25):
+    if not two_sum_next(data[idx : idx + 25], data[idx + 25]):
+        answer = data[idx + 25]
+        break
+found = True
 
 ### END SOLUTION BODY ###
 
