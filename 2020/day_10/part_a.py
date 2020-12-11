366e743251c54d20f544d329b0e84dc07c4d524e
delmitch <amitchell@alaska.edu>
2020-12-10 19:11:39 -0900

    Refactor: updates loop style

--- 2020/day_10/part_a.py
+++ 2020/day_10/part_a.py
@@ -7,12 +7,11 @@ data = utils.lines_to_ints('input_files/aoc_10.txt')
 
 # find the product of the number of 1- and 3-volt differences
 found, answer = False, 0
-data.sort()
 ones, threes = 1, 1
-for idx in range(len(data) - 1):
-    if data[idx + 1] == data[idx] + 1:
+for item in data:
+    if item + 1 in data:
         ones += 1
-    elif data[idx + 1] == data[idx] + 3:
+    elif item + 3 in data:
         threes += 1
 answer = ones * threes
 found = True
