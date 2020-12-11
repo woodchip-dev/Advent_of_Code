3987be86030bf6691a64ee2c8750535c2749d829
delmitch <amitchell@alaska.edu>
2020-12-10 19:12:14 -0900

    Feat: solves day 10 part b

--- 2020/day_10/part_b.py
+++ 2020/day_10/part_b.py
@@ -1,13 +1,26 @@
 import utils
 
 # fetch data
-data = utils.line_to_list('input_files/aoc_xx.txt')
+data = utils.lines_to_ints('input_files/aoc_10.txt')
+
+def count_arrangements(data, curr, storage):
+    count = 0
+    if curr == max(data):
+        return 1
+    for idx in range(curr + 1, curr + 4):
+        if idx in data:
+            if not idx in storage:
+                storage[idx] = count_arrangements(data, idx, storage)
+            count += storage[idx]
+    return count
 
 ### START SOLUTION BODY ###
 
-# task overview comment
-found, answer = False, None
-# add code here
+# find the total number of distinct ways to arrange the adapters
+found, answer = False, 0
+data.sort()
+answer = count_arrangements(data, 0, {})
+found = True
 
 ### END SOLUTION BODY ###
 
