ddf21ff710f8a1b04563fb1604e45ebb48c8d3d2
delmitch <amitchell@alaska.edu>
2020-12-13 13:24:58 -0900

    Feat: solves day 13 part a

--- 2020/day_13/part_a.py
+++ 2020/day_13/part_a.py
@@ -1,13 +1,20 @@
-import utils
+import utils, math
 
 # fetch data
-data = utils.line_to_list('input_files/aoc_xx.txt')
+data = utils.line_to_list('input_files/aoc_13.txt')
 
 ### START SOLUTION BODY ###
 
-# task overview comment
+# find the product of the bus ID I'll take and the minutes I need to wait for it
 found, answer = False, None
-# add code here
+buses = [int(bus) for bus in data[1].split(',') if not bus == 'x']
+departures = []
+for bus in buses:
+    start = 0
+    departures.append((bus, bus * math.ceil(int(data[0]) / bus)))
+departure = min(departures, key = lambda t: t[1])
+answer = departure[0] * (departure[1] - int(data[0]))
+found = True
 
 ### END SOLUTION BODY ###
 
