09763eda23ed7c977a7146fa9ef62f6dd5bb3a73
delmitch <amitchell@alaska.edu>
2020-12-14 09:07:22 -0900

    Feat: solves day 13 part b for small input; so slow, snails would be jealous

--- 2020/day_13/part_b.py
+++ 2020/day_13/part_b.py
@@ -1,13 +1,32 @@
 import utils
 
 # fetch data
-data = utils.line_to_list('input_files/aoc_xx.txt')
+data = utils.line_to_list('input_files/aoc_13.txt')
 
 ### START SOLUTION BODY ###
 
-# task overview comment
+# find the earliest timestamp such that all of the bus IDs depart at offsets matching their positions in the list
 found, answer = False, None
-# add code here
+time_table = data[1].split(',')
+buses = [int(bus) for bus in time_table if not bus == 'x']
+index = [i for i in range(len(time_table)) if not time_table[i] == 'x']
+schedules = list(map(lambda i, b : (i, b), index, buses))
+idx, time = 0, 0
+clocked = []
+while idx < len(schedules):
+    offset = 1
+    print(time)
+    if time % schedules[idx][1] == 0:
+        clocked.append(time)
+        idx +=1
+        if idx < len(schedules):
+            offset = schedules[idx][0] - schedules[idx - 1][0]
+    else:
+        idx = 0
+        clocked = []
+    time += offset
+answer = clocked[0]
+found = True
 
 ### END SOLUTION BODY ###
 
