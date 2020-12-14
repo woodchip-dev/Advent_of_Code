d6183d8b61c7c514fa79f47de9ef283f5de1a08e
delmitch <amitchell@alaska.edu>
2020-12-13 20:43:10 -0900

    Refactor: removes unused var

--- 2020/day_13/part_a.py
+++ 2020/day_13/part_a.py
@@ -6,11 +6,10 @@ data = utils.line_to_list('input_files/aoc_13.txt')
 ### START SOLUTION BODY ###
 
 # find the product of the bus ID I'll take and the minutes I need to wait for it
-found, answer = False, None
+found, answer = False, 0
 buses = [int(bus) for bus in data[1].split(',') if not bus == 'x']
 departures = []
 for bus in buses:
-    start = 0
     departures.append((bus, bus * math.ceil(int(data[0]) / bus)))
 departure = min(departures, key = lambda t: t[1])
 answer = departure[0] * (departure[1] - int(data[0]))
