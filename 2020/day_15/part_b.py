30d677d05e6f805652719d5361817bb0b53f80d6
delmitch <amitchell@alaska.edu>
2020-12-14 23:57:03 -0900

    Feat: solves day 15 part b

--- 2020/day_15/part_b.py
+++ 2020/day_15/part_b.py
@@ -1,13 +1,20 @@
 import utils
 
 # fetch data
-data = utils.line_to_list('input_files/aoc_xx.txt')
+data = utils.commas_to_list('input_files/aoc_15.txt')
 
 ### START SOLUTION BODY ###
 
-# task overview comment
+# find the 30000000th number spoken given the rules
 found, answer = False, None
-# add code here
+record = {int(last): i + 1 for i, last in enumerate(data)}
+last = int(data[-1])
+for idx in range(len(data), 30000000):
+    curr = idx - record[last] if last in record else 0
+    record[last] = idx
+    last = curr
+answer = curr
+found = True
 
 ### END SOLUTION BODY ###
 
