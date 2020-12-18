a276d13bf44929ebe4633c6929d03ab5c6654411
delmitch <amitchell@alaska.edu>
2020-12-18 00:33:38 -0900

    Feat: solves day 18 part b

--- 2020/day_18/part_b.py
+++ 2020/day_18/part_b.py
@@ -1,13 +1,54 @@
-import utils
+import utils, re
 
 # fetch data
-data = utils.line_to_list('input_files/aoc_xx.txt')
+data = utils.line_to_list('input_files/aoc_18.txt')
 
 ### START SOLUTION BODY ###
 
-# task overview comment
-found, answer = False, None
-# add code here
+def calculate(expr):
+    while 1:
+        parens = re.findall('\\([0-9\\+\\*]+\\)', expr)
+        if not parens == []:
+            for par in parens:
+                new_val = calculate(par[1:-1])
+                expr = expr.replace(par, str(new_val))
+        else:
+            break
+    while 1:
+        pluses = re.findall('[0-9]+\\+[0-9]+', expr)
+        if not pluses == []:
+            new_val = calculate(pluses[0].replace('+', '#'))
+            expr = expr.replace(pluses[0], str(new_val))
+        else:
+            break
+    temp = 0
+    plus, mult = False, False
+    expr = expr.replace('*', ' * ')
+    expr = expr.replace('+', ' + ')
+    expr = expr.replace('#', ' # ')
+    expr = expr.split(' ')
+    for ch in expr:
+        if ch == '+' or ch == '#':
+            plus = True
+        elif ch == '*':
+            mult = True
+        elif ch.isdigit():
+            if not plus and not mult:
+                temp = int(ch)
+            elif plus and not mult:
+                temp += int(ch)
+                plus = False
+            elif not plus and mult:
+                temp *= int(ch)
+                mult = False
+    return temp
+
+# find the sum of the results of each equation given the precedent rules
+found, answer = False, 0
+data = [item.replace(' ', '') for item in data]
+for item in data:
+    answer += calculate(item)
+found = True
 
 ### END SOLUTION BODY ###
 
