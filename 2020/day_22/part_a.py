5c964d068392a57f6be1611a47d1c6c05338366d
delmitch <amitchell@alaska.edu>
2020-12-21 23:35:08 -0900

    Feat: solves day 22 part a

--- 2020/day_22/part_a.py
+++ 2020/day_22/part_a.py
@@ -1,13 +1,32 @@
 import utils
 
 # fetch data
-data = utils.line_to_list('input_files/aoc_xx.txt')
+data = utils.different_groups('input_files/aoc_22.txt')
 
 ### START SOLUTION BODY ###
 
-# task overview comment
-found, answer = False, None
-# add code here
+# find the score of the winning player
+found, answer = False, 0
+player1, player2 = [int(p1) for p1 in data[0][1:]], [int(p2) for p2 in data[1][1:]]
+while len(player1) > 0 and len(player2) > 0:
+    if player1[0] > player2[0]:
+        player1.append(player1[0])
+        player1.append(player2[0])
+        del player1[0]
+        del player2[0]
+    else:
+        player2.append(player2[0])
+        player2.append(player1[0])
+        del player1[0]
+        del player2[0]
+winner = None
+if player1 == []:
+    winner = player2
+else:
+    winner = player1
+for item in winner:
+    answer += item * (len(winner) - winner.index(item))
+found = True
 
 ### END SOLUTION BODY ###
 
