e13ac71a0ba5675886e46225dadb104347f76a5b
delmitch <amitchell@alaska.edu>
2020-12-23 10:15:23 -0900

    Refactor: hides 'already up-to-date' message

--- 2020/setup.sh
+++ 2020/setup.sh
@@ -2,6 +2,6 @@
 
 source env/bin/activate
 
-pip3 install --upgrade wheel
+pip3 install --upgrade wheel | grep -v 'already up-to-date'
 
 pip3 install -r requirements.txt | grep -v 'already satisfied'
