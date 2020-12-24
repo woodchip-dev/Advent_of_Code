33b53234c7f310181eb28723113d4ea86727269a
delmitch <amitchell@alaska.edu>
2020-12-23 20:30:11 -0900

    Feat: adds initial day 24 files

---
+++ 2020/day_24/part_a.py
@@ -0,0 +1,16 @@
+import utils
+
+# fetch data
+data = utils.line_to_list('input_files/aoc_xx.txt')
+
+### START SOLUTION BODY ###
+
+# task overview comment
+found, answer = False, None
+# add code here
+
+### END SOLUTION BODY ###
+
+# output result
+utils.one_val_out(found, answer)
+
