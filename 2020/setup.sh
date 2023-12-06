95d78fb32e8088515329656dba035b0d21a028da
woodchip <woodchip.dev@gmail.com>
2023-12-06 14:47:17 -0800

    Chore: removes unneeded files

--- 2020/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source env/bin/activate
-
-pip3 install --upgrade wheel
-
-pip3 install -r requirements.txt | grep -v 'already satisfied'
--- 2020/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .input_handler import line_to_list
-from .input_handler import lines_to_ints
-from .input_handler import commas_to_list
-from .input_handler import congruous_groups
-from .input_handler import different_groups
-from .output_handler import one_val_out
